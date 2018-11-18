6199c7467b41f240a111e0e46265b653e5c44e8d
Erick Menezes <erickbmenezes@gmail.com>
2018-11-18 11:07:22 -0200

    adicionando novas informacões na resposta da busca por produto

--- app/controllers/product.ts
+++ app/controllers/product.ts
@@ -7,34 +7,35 @@ import { Product } from '../models/Product';
 export default class ProductController {
   get = (req: Request, res: Response, next: Next) => {
     let query = req.query;
-    let where = Object.assign({}, query, {})
-
-    delete where.show;
-    delete query.page;
-    delete where.page;
-
     let limit = parseInt(query.limit) || 8;
     let page = parseInt(query.page) || 1;
 
-    Product.find(query, null, {skip: (page - 1) * limit, limit: limit}, (err, products) => {
-      if (err) {
-        return next(err);
-      }
+    let where = Object.assign({}, query, {})
 
-      let count = products.length;
+    delete where.limit;
+    delete where.page;
+    delete query.page;
 
-      let qs = querystring.stringify(query);
-      qs = Object.keys(query).length > 0 ? '&' + qs : qs;
-      let paginationUrl = '/products?page=';
-
-      res.json({
-        count: count, 
-        page: page,
-        previous: page === 1 ? null : paginationUrl + (page - 1) + qs,
-        next: count - (page * limit) <= 0 ? null : paginationUrl + (page + 1) + qs,
-        results: products
+    Product.count(where, function(err, count) {
+      Product.find(where, null, {skip: (page - 1) * limit, limit: limit}, (err, products) => {
+        if (err) {
+          return next(err);
+        }
+  
+        let qs = querystring.stringify(query);
+        qs = Object.keys(query).length > 0 ? '&' + qs : qs;
+  
+        let paginationUrl = '/products?page=';
+        res.json({
+          count: count, 
+          page: page,
+          totalPages: Math.ceil(count/limit),
+          previous: page === 1 ? null : paginationUrl + (page - 1) + qs,
+          next: count - (page * limit) <= 0 ? null : paginationUrl + (page + 1) + qs,
+          results: products
+        });
+        next();
       });
-      next();
     });
   }
 }
